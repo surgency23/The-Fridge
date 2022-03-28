140accb41e9196b0e7e23a8da9c8eaf592f3885b
Alex Pappa <alex.pappa.1@gmail.com>
2022-03-27 23:49:10 -0400

    dark mode toggle. put elsewhere

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,43 @@ import 'package:flutter/material.dart';
 import '../test_data/recipes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 
-  // This widget is the root of your application.
+  /// ↓↓ ADDED
+  /// InheritedWidget style accessor to our State object.
+  static _MyAppState? of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>();
+}
+
+/// State object hidden ↓. Focusing on ↑ StatefulWidget here.
+
+class _MyAppState extends State<MyApp> {
+  /// 1) our themeMode "state" field
+  ThemeMode _themeMode = ThemeMode.system;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        /* dark theme settings */
-      ),
-      //themeMode: ThemeMode.dark,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode, // 2) ← ← ← use "state" field here //////////////
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
+  }
+
+  /// 3) Call this to change theme from any context using "of" accessor
+  /// e.g.:
+  /// MyApp.of(context).changeTheme(ThemeMode.dark);
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
   }
 }
 
@@ -39,18 +55,41 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ListView(
+            children:
+                _recipes.map((e) => Center(child: Text(e.title))).toList()),
+      ),
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Choose your theme:',
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                /// //////////////////////////////////////////////////////
+                /// Change theme & rebuild to show it using these buttons
+                ElevatedButton(
+                    onPressed: () =>
+                        MyApp.of(context)?.changeTheme(ThemeMode.light),
+                    child: const Text('Light')),
+                ElevatedButton(
+                    onPressed: () =>
+                        MyApp.of(context)?.changeTheme(ThemeMode.dark),
+                    child: const Text('Dark')),
+
+                /// //////////////////////////////////////////////////////
+              ],
+            ),
+          ],
         ),
-        body: Center(
-          child: ListView(
-              children:
-                  _recipes.map((e) => Center(child: Text(e.title))).toList()),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add your onPressed code here!
-          },
-        ));
+      ),
+    );
   }
 }
