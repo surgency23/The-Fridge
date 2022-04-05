65f47e662e1a8b2e08064752519a85606e6b13bb
Alex <alex.pappa.1@gmail.com>
2022-04-04 21:49:45 -0400

    update

--- lib/widgets/IngredientsView.dart
+++ lib/widgets/IngredientsView.dart
@@ -4,6 +4,28 @@ import 'package:expand_widget/expand_widget.dart';
 class IngredientsView extends StatelessWidget {
   final List<String> ingredients;
   IngredientsView(this.ingredients);
+  List<List<String>> ingredientSplitter(arr, numberOfArrs) {
+    List<int> sumOfArrs = List<int>.filled(numberOfArrs, 0);
+    List<List<String>> returnList = List.generate(numberOfArrs, (i) => []);
+    arr.sort((String a, String b) => a.compareTo(b));
+    for (String str in arr) {
+      int lowestIndex = 0;
+      for (int index = 0; index < sumOfArrs.length - 1; index++) {
+        if (sumOfArrs[index] < sumOfArrs[index + 1]) {
+          lowestIndex = index;
+        } else {
+          lowestIndex = index + 1;
+        }
+      }
+      returnList[lowestIndex].add(str);
+      sumOfArrs[lowestIndex] += str.length;
+    }
+    return returnList;
+  }
+
+  Widget ingredientTextBoxes(numberOfBoxes) {
+    throw ("stop");
+  }
 
   @override
   Widget build(BuildContext context) {
